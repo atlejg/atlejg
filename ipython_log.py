c3ac4d4c86a4ae8b82a1f0a53fb02adabde57346
Atle Johnsen Gyllensten <agy@statoil.com>
2018-08-08 10:08:17 +0200

    update

---
+++ ipython_log.py
@@ -0,0 +1,7 @@
+# IPython log file
+
+
+sys.path
+[x for x in sys.path if 'atlejg' in x]
+get_ipython().magic(u'ls /private/agy/Tools/atlejg/lib/Python')
+get_ipython().magic(u'ls /private/agy/Tools/lib/Python')
