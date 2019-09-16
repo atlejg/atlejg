2bd534683d7bed41ffefde4ada75c7b3e9b883d8
Atle Johnsen Gyllensten (ST RRR PT) <agy@st-linrgs092.st.statoil.no>
2019-09-16 10:38:47 +0200

    cleaning

--- lib/AtlejgTools/Plotting/timetest.py
+++
@@ -1,9 +0,0 @@
-import random
-from datetime import datetime, timedelta
-
-today = datetime.now()
-
-dates = [today + timedelta(days=i) for i in range(10)]
-values = [random.randint(1, 20) for i in range(10)]
-plot_date(date2num(dates), values, linestyle='-')
-
--- lib/AtlejgTools/SimulationTools/ipython_log.py
+++
@@ -1,7 +0,0 @@
-# IPython log file
-
-
-get_ipython().magic(u'pwd ')
-get_ipython().magic(u'pwd ')
-get_ipython().magic(u'pwd ')
-get_ipython().magic(u'pwd ')
