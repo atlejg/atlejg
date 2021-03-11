45c8961d1cbbf343eab408d7224c8f38711e4721
Atle Johnsen Gyllensten <agy@statoil.com>
2021-03-11 09:30:06 +0100

    removing coefficients

--- lib/Python3/AtlejgTools/RCP_project/Valve_characteristics.py
+++ lib/Python3/AtlejgTools/RCP_project/Valve_characteristics.py
@@ -129,35 +129,8 @@ def icd_dp2(gor, rho_o, rho_g, mu_o, mu_g, cnst, q, rho_cal=_RHO_CAL, mu_cal=_MU
     rho = so*rho_o + (1-so)*rho_g
     return icd_dp(rho, mu, cnst, q, rho_cal, mu_cal)
 
-def ar3_dp(rho, mu, q):
-    '''
-    as of 24/11-2010, based on oseberg FFR-tests
-    [rho] = kg/m3
-    [mu]  = cp
-    [q]   = l/h
-    [dp]  = bar
-    '''
-    return rcp_dp(rho, mu, 750., 0.79, 3.5e-04, 2.2, 0.58, q)
 
-def ar7_dp(rho, mu, q):
-    '''
-    as of 24/11-2010, based on oseberg FFR-tests
-    [rho] = kg/m3
-    [mu]  = cp
-    [q]   = l/h
-    [dp]  = bar
-    '''
-    return rcp_dp(rho, mu, 750., 0.79, 1.25e-05, 2.7, 0.95, q)
 
-def ar7_flowr(rho, mu, dp):
-    '''
-    as of 24/11-2010, based on oseberg FFR-tests
-    [rho] = kg/m3
-    [mu]  = cp
-    [q]   = l/h
-    [dp]  = bar
-    '''
-    return rcp_flowr(rho, mu, 750., 0.79, 1.25e-05, 2.7, 0.95, dp)
 
 def icd_strength(bar_strength):
     '''
@@ -221,75 +194,6 @@ def fluent_sicd_coefficients(area, length, rho, mu, bar_strength):
         print("warning: c0 = %e > 1e20 (which is max limit for fluent gui-input)" % c0)
     return (c0, c1)
 
-def rcp_params(valve_type):
-    valve_type = valve_type.lower()
-    ## official versions
-    if   valve_type == 'ar3':         # should use ar3_oseberg
-        rho_cal = 750.; mu_cal = 0.79; a_aicd = 3.50e-04; x = 2.2; y = 0.58
-    elif valve_type == 'ar7':         # should use ar7_oseberg
-        rho_cal = 750.; mu_cal = 0.79; a_aicd = 1.25e-05; x = 2.7; y = 0.95
-    elif valve_type == 'ar3_oseberg':
-        rho_cal = 750.; mu_cal = 0.79; a_aicd = 3.50e-04; x = 2.2; y = 0.58
-    elif valve_type == 'ar3_grane':   # this is the recommended version
-        rho_cal = 900.; mu_cal = 12.; a_aicd = 2e-07; x = 4.6; y = 0.8
-    elif valve_type == 'ar7_grane':   # this is the recommended version
-        rho_cal = 865.; mu_cal = 12.; a_aicd = 7e-07; x = 3.3; y = 0.9
-    elif valve_type == 'ar7_oseberg':
-        rho_cal = 750.; mu_cal = 0.79; a_aicd = 1.25e-05; x = 2.7; y = 0.95
-    elif valve_type == 'ar3_peregrino':
-        rho_cal = 1020.; mu_cal = 0.48; a_aicd = 4.0e-05; x = 2.8; y = 0.52
-    elif valve_type == 'ar7_bressay':
-        rho_cal = 1020.; mu_cal = 0.48; a_aicd = 1.0e-04; x = 2.3; y = 0.7
-    elif valve_type == 'ar7_peregrino_1': # this is 'best fit'
-        rho_cal = 1020.; mu_cal = 0.48; a_aicd = 1.4e-05; x = 3.0; y = 0.7
-    elif valve_type == 'ar7_peregrino_2': # this is 'lo dP oil'
-        rho_cal = 1020.; mu_cal = 0.48; a_aicd = 7.0e-05; x = 2.4; y = 0.75
-    elif valve_type == 'ar7_peregrino_3': # this is 'hi dP oil'
-        rho_cal = 1020.; mu_cal = 0.48; a_aicd = 4.5e-05; x = 4.5; y = 0.9
-    elif valve_type == 'ar7_peregrino_dz05': # this is the hard-metal case. first try..
-        rho_cal = 1000.; mu_cal = 1.00; a_aicd = 3.33e-05; x = 2.34; y = 0.48
-    elif valve_type == 'tr7_peregrino': # this is the tendeka version for peregrino
-        rho_cal = 1000.; mu_cal = 1.00; a_aicd = 1.6e-05; x = 2.4; y = 0.5
-    elif valve_type == 'tr7-3_200cp': # this is the tendeka version developed for NK (3mm inlet), 200cp
-        rho_cal = 1000.; mu_cal = 1.00; a_aicd = 1.80e-03; x = 1.60; y = 0.40
-    elif valve_type == 'tr7_troll': # this is the tendeka version for troll
-        rho_cal = 1000.; mu_cal = 1.00; a_aicd = 2.00e-05; x = 2.80; y = 0.70
-    elif valve_type == 'tr7_petrocedeno': # this is the tendeka version for petrocedeno
-        rho_cal = 1000.; mu_cal = 1.00; a_aicd = 1.30e-03; x = 1.50; y = 0.20
-    ## experimental versions:
-    elif valve_type == 'ar3__peregrino_v2':
-        rho_cal = 1020.; mu_cal = 0.48; a_aicd = 1.3e-04; x = 3.3; y = 1.0
-    elif valve_type == 'ar3__peregrino_v3':
-        rho_cal = 1020.; mu_cal = 0.48; a_aicd = 4.0e-04; x = 3.3; y = 1.2
-    elif valve_type == 'ar3__peregrino_v4':
-        rho_cal = 1020.; mu_cal = 0.48; a_aicd = 0.05;    x = 0.0; y = 0.2
-    ## systematic testing...
-    elif valve_type == 'ar_x=3_y=0':
-        rho_cal = 1020.; mu_cal = 0.48; a_aicd = 0.05;    x = 3.0; y = 0.0
-    elif valve_type == 'ar_x=2_y=0':
-        rho_cal = 1020.; mu_cal = 0.48; a_aicd = 0.05;    x = 2.0; y = 0.0
-    elif valve_type == 'ar_x=1_y=0':
-        rho_cal = 1020.; mu_cal = 0.48; a_aicd = 0.05;    x = 1.0; y = 0.0
-    elif valve_type == 'ar_x=3_y=0.2':
-        rho_cal = 1020.; mu_cal = 0.48; a_aicd = 0.05;    x = 3.0; y = 0.2
-    elif valve_type == 'ar_x=3_y=0.4':
-        rho_cal = 1020.; mu_cal = 0.48; a_aicd = 0.05;    x = 3.0; y = 0.4
-    elif valve_type == 'ar_x=3_y=0.7':
-        rho_cal = 1020.; mu_cal = 0.48; a_aicd = 0.05;    x = 3.0; y = 0.7
-    elif valve_type == 'ar_x=3_y=1.0':
-        rho_cal = 1020.; mu_cal = 0.48; a_aicd = 0.05;    x = 3.0; y = 1.0
-    elif valve_type == 'ar_x=0_y=0.5':
-        rho_cal = 1020.; mu_cal = 0.48; a_aicd = 0.05;    x = 0.0; y = 0.5
-    elif valve_type == 'ar_x=0_y=1.0':
-        rho_cal = 1020.; mu_cal = 0.48; a_aicd = 0.05;    x = 0.0; y = 1.0
-    # non-RCP valves
-    elif valve_type == 'bh_equalselect_6.4':
-        # should not be used with gas!!
-        rho_cal = 1000.; mu_cal = 1.; a_aicd = 7.3e-06; x = 2.0; y = -0.025
-    elif valve_type == 'halib_how_r3':
-        rho_cal = 1020.; mu_cal = 0.48; a_aicd = 2.75e-4; x = 2.2; y = 0.325
-    else : raise Exception("no such valve_type (%s)" % valve_type)
-    return (rho_cal, mu_cal, a_aicd, x, y)
 
 def fluent_rcp_coefficients(area, length, rho, mu, valve_type=None, rho_cal=0., mu_cal=0., x=0., y=0., a_aicd=0.):
     '''
