79583bae0146e8d8bde11783d85f819b5c066891
Atle Johnsen Gyllensten <AGY@equinor.com>
2021-10-26 12:46:07 +0200

    updating setup.py

--- setup.py
+++ setup.py
@@ -7,17 +7,23 @@ with open("LICENSE") as f:
     project_license = f.read()
 
 with open("requirements.txt") as f:
-    requirements = [line.split('=')[0] for line in f.readlines()]
+    requirements = [line.strip() for line in f.readlines()]
+    requirements_clean = [req.split('=')[0] for req in requirements]  # without '==version'
 
 setuptools.setup(
-    name="atlejg",
+    #
+    name="AtlejgTools",
+    package_dir = {'': 'lib/Python3'},
+    packages=setuptools.find_packages('lib/Python3'),
+    scripts=[ "lib/Python3/AtlejgTools/Scripts/run_pywake.py"],
+    #
+    install_requires=requirements,
+    #install_requires=requirements_clean,
+    #
     version="0.1.0",
     description="atle's stuff",
     long_description=readme,
     author="Equinor ASA",
     author_email="fg_sib-scout@equinor.com",
     url="https://github.com/atlejg/atlejg",
-    packages=setuptools.find_packages(),
-    install_requires=requirements,
-    scripts=[ "lib/Python3/AtlejgTools/Scripts/run_pywake.py"],
 )
